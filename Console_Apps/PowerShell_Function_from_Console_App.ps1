e25738520d520994d1055fc59cbaa38d35a58f08
Wesley Kirkland <wesley@wesleyk.me>
2017-09-24 11:06:00 -0400

    Starting to build index for parameters with name and basic help

--- Console_Apps/PowerShell_Function_from_Console_App.ps1
+++ Console_Apps/PowerShell_Function_from_Console_App.ps1
@@ -2,13 +2,15 @@
 $SectionPatterns = "^::.*|^##.*" #This pattern matches on section header starts
 $SectionPatternCharactersToReplace = "[:|#]" #This pattern matches on characters to replace when sanatizing the section header
 [System.Collections.ArrayList]$SectionHeaderVariables = @()
-$VerbosePreference = 'Continue'
+#Line matching
+$LinePatterns = "^/.*"
+$VerbosePreference = 'Continue' #Debugging my code
 
 #Location of the binary file we want to scan
 Set-Location 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy'
 
-#Binary we want to convert
-$BinaryHelpInfo = Robocopy.exe /?
+#Binary we want to convert, you know I'm really developing a hatred for legacy console applications
+$BinaryHelpInfo = .\AzCopy.exe /?
 
 #Basic sanitization of the help data, removes excess lines and leading/trailing spaces
 $BinaryHelpInfo = $BinaryHelpInfo | Where-Object {$_}
@@ -43,7 +45,11 @@ for ($i = 0; $i -lt $BinaryHelpInfo.Count; $i++) {
                     New-Variable $SectionVariableName
                     $SectionVariableCreated = $true
                     Try {
-                        $SectionHeaderVariables.Add($SectionVariableName) | Out-Null
+                        $SectionHeaderNameObject = New-Object -TypeName psobject
+                        $SectionHeaderNameObject | Add-Member -MemberType NoteProperty -Name 'SectionVariable' -Value $SectionVariableName
+                        $SectionHeaderNameObject | Add-Member -MemberType NoteProperty -Name 'HeaderName' -Value $Script:SectionHeaderName
+
+                        $SectionHeaderVariables.Add($SectionHeaderNameObject) | Out-Null
                     } Catch {}
                     $i = $i - 1 #Step the loop back int 1 so we don't miss the line
                 } else {
@@ -62,21 +68,28 @@ for ($i = 0; $i -lt $BinaryHelpInfo.Count; $i++) {
             (!($BinaryHelpInfoHeaderMatch)) -and
             ($SectionHeaderVariables) #So we don't run this block on the first few lines
         ) {
-        #Populate the Section Variable with the Header Name
-        if ($Script:SectionHeaderName) {
-            Set-Variable -Name $SectionVariableName -Value @(
-                "Header Name: $Script:SectionHeaderName"
-                ''
-            )
-            $Script:SectionHeaderName = $null #Nullify it so we forget about it
-        }
 
         #Populate the Section variable with content
         Set-Variable -Name $SectionVariableName -Value @(
             (Get-Variable -Name $SectionVariableName).Value #Add the existing value so we don't lose it
             $BinaryHelpInfo[$i] #Add new line info
         )
+    } #end Normal Line elseif
+} #End for loop
+
+#Find all sections that have actual options available
+$SectionHeaderVariablesWithOptions = $SectionHeaderVariables | Where-Object {($PSItem.HeaderName -like "*Options*")}
+
+foreach ($Section in $SectionHeaderVariablesWithOptions) {
+    $Lines = (Get-Variable -Name $Section.SectionVariable).Value | Where-Object {$PSItem} #Get the value and remove blank lines
+
+    foreach ($Line in $Lines) {
+        if ($Line -match $LinePatterns) {
+           $LineSplit = ($Line.Split(':') -replace '/','').Trim()
+
+           $ParameterToAdd = New-Object -TypeName psobject
+           $ParameterToAdd | Add-Member -MemberType NoteProperty -Name 'ParameterName' -Value $LineSplit[0]
+           $ParameterToAdd | Add-Member -MemberType NoteProperty -Name 'ParameterHelp' -Value (($Line.Split(':') -replace '/','').Trim()[-1] -replace '<*.*>','').Trim()
+        }
     }
 }
-
-$Section1
